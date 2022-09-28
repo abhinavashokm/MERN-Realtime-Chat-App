7cfa9497f97430ae22a58fa763bcb9e0c63f7336
Abhinav <abhirealme55@gmail.com>
2022-09-28 12:38:55 +0530

    socket.io setup done

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,12 @@
-
+import React, { useEffect, useState } from 'react';
+import {io} from 'socket.io-client';
 
 function App() {
+const [socket, setSocket] = useState()
+  useEffect(() => {
+    setSocket(io("ws://localhost:3001"))
+  }, [])
+  
   return (
     <div className="App">
       <h1>hello world</h1>
--- server/index.js
+++ server/index.js
@@ -1,11 +1,28 @@
 const express = require('express')
+const http = require("http");
+const { Server } = require("socket.io");
+
+// MONGODB CONNCETION
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost:27017/ChatApp')
+
+//SOCKET.IO SETUP
 const app = express()
-const mongoose =  require('mongoose')
-
-mongoose.connect('mongodb://localhost:27017/ChatApp').then(()=> {
-    console.log("CONNECTED TO DATABASE SUCCESSFULLY!")
+const httpServer = http.createServer(app)
+const io = new Server(httpServer, {
+    cors: {
+        origin: "http://localhost:3000",
+    }
 })
 
-app.listen(3001,() => {
+//WHEN NEW CONNCTION ESTABLISED
+io.on("connection", (socket) => {
+    console.log("new connection establised")
+})
+
+//PORT LISTENING
+httpServer.listen(3001, () => {
     console.log('SERVER STARTED!')
-})
+});
+
+
